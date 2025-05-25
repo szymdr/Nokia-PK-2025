7d03b01f71d157600b268d7678e196d8fe73ca1b
Szymon Dral <szymon.dral@gmail.com>
2025-05-25 19:12:07 +0200

    add timer implementation

--- src/UE/Application/Ports/TimerPort.cpp
+++ src/UE/Application/Ports/TimerPort.cpp
@@ -1,4 +1,7 @@
+// c++
 #include "TimerPort.hpp"
+#include <chrono>
+#include <thread>
 
 namespace ue
 {
@@ -6,6 +9,15 @@ namespace ue
 TimerPort::TimerPort(common::ILogger &logger)
     : logger(logger, "[TIMER PORT]")
 {}
+
+TimerPort::~TimerPort()
+{
+    stopTimer();
+    if (timerThread.joinable())
+    {
+        timerThread.join();
+    }
+}
 
 void TimerPort::start(ITimerEventsHandler &handler)
 {
@@ -15,18 +27,45 @@ void TimerPort::start(ITimerEventsHandler &handler)
 
 void TimerPort::stop()
 {
-    logger.logDebug("Stoped");
+    logger.logDebug("Stopped");
+    stopTimer();
     handler = nullptr;
 }
 
 void TimerPort::startTimer(Duration duration)
 {
     logger.logDebug("Start timer: ", duration.count(), "ms");
+    stopTimer();
+
+    std::thread(&TimerPort::startCountdown, this, duration).detach();
+}
+
+void TimerPort::startCountdown(Duration duration)
+{
+    logger.logDebug("Start countdown timer: ", duration.count(), "ms");
+    stopTimer();
+    timerRunning.store(true);
+    timerThread = std::thread([this, duration]() {
+        auto startTime = std::chrono::steady_clock::now();
+        while (timerRunning.load() && std::chrono::steady_clock::now() - startTime < duration)
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
+        if (timerRunning.load() && handler)
+        {
+            handler->handleTimeout();
+        }
+        timerRunning.store(false);
+    });
 }
 
 void TimerPort::stopTimer()
 {
     logger.logDebug("Stop timer");
+    timerRunning.store(false);
+    if (timerThread.joinable())
+    {
+        timerThread.join();
+    }
 }
-
 }
--- src/UE/Application/Ports/TimerPort.hpp
+++ src/UE/Application/Ports/TimerPort.hpp
@@ -2,6 +2,8 @@
 
 #include "ITimerPort.hpp"
 #include "Logger/PrefixedLogger.hpp"
+#include <thread>
+#include <atomic>
 
 namespace ue
 {
@@ -10,17 +12,21 @@ class TimerPort : public ITimerPort
 {
 public:
     TimerPort(common::ILogger& logger);
+    ~TimerPort() override;
 
     void start(ITimerEventsHandler& handler);
     void stop();
 
     // ITimerPort interface
     void startTimer(Duration duration) override;
+    void startCountdown(Duration duration) override;
     void stopTimer() override;
 
 private:
     common::PrefixedLogger logger;
     ITimerEventsHandler* handler = nullptr;
+    std::thread timerThread;
+    std::atomic<bool> timerRunning{false};
 };
 
 }
